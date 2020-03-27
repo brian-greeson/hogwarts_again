48c028f4f27134ecb09642cc1d3a296ec7b0dda9
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-03-27 10:33:52 -0600

    Add student_average_age method and update show view

--- app/models/professor.rb
+++ app/models/professor.rb
@@ -1,5 +1,13 @@
-class Professor <ApplicationRecord
-validates_presence_of :name, :age, :specialty
-has_many :professor_students
-has_many :students,  through: :professor_students
+class Professor < ApplicationRecord
+  validates_presence_of :name, :age, :specialty
+  has_many :professor_students
+  has_many :students,  through: :professor_students
+
+  def students_average_age
+    total_age = students.reduce([]) do |student_ages,student|
+      student_ages <<  student.age
+      student_ages
+    end.sum
+    total_age.to_f / students.count
+  end
 end

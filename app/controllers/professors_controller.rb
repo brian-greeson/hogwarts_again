92c2b36ced692f7905ed7f2d5a9eb913b55daf19
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-03-27 09:33:57 -0600

    Created Professor index mvc

--- app/controllers/professors_controller.rb
+++ app/controllers/professors_controller.rb
@@ -1,5 +1,5 @@
 class ProfessorsController < ApplicationController
   def index
-    # code
+    @professors = Professor.all
   end
 end
--- spec/features/professors/index_spec.rb
+++ spec/features/professors/index_spec.rb
@@ -7,8 +7,13 @@ RSpec.describe "When a user" do
 
     visit "/professors"
 
-    expe
-    binding.pry
+    expect(page).to have_content(snape.name)
+    expect(page).to have_content(snape.age)
+    expect(page).to have_content(snape.specialty)
+    expect(page).to have_content(hagarid.name)
+    expect(page).to have_content(hagarid.age)
+    expect(page).to have_content(hagarid.specialty)
+    
 
   end
 
